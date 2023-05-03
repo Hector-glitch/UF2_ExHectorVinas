d59e8724c82d6d18e951e71a744e4331c2079978
Héctor <hector.vinas@institutvidreres.cat>
2023-05-03 09:58:04 +0200

    Aaawfsdfvc<

--- server.js
+++ server.js
@@ -31,13 +31,13 @@ async function modifCorreu(idAlumne) {
   const [rows] = await connection.execute('SELECT * FROM alumnes WHERE ALUMN_DNI = ?', [idAlumne]);
   const alumne = rows[0];
 
-  // Verificar si el camp correu electrònic ja és un VARCHAR(30)
+  // Verificar si el camp correu electrònic ja és un VARCHAR (30)
   if (alumne.ALUMN_E_MAIL.length === 30 && alumne.ALUMN_E_MAIL.type === 'VARCHAR(30)') {
     console.log('El camp correu electrònic ja és un VARCHAR(30)');
     return;
   }
 
-  // Modificar la columna correu electrònic a VARCHAR(30)
+  // Modificar la columna correu electrònic a VARCHAR (30)
   await connection.execute('ALTER TABLE alumnes MODIFY COLUMN ALUMN_E_MAIL VARCHAR(30)');
 
   console.log('El camp correu electrònic s\'ha modificat correctament a VARCHAR(30)');
@@ -46,39 +46,61 @@ async function modifCorreu(idAlumne) {
 
 //Exercici 2
 
-async function getCodiDepartament(nomDepartament, connection) {
-  const [rows] = await connection.execute('SELECT DEPT_CODI FROM departaments WHERE nom = NOM_DEPT', [nomDepartament]);
-  if (rows.length === 0) {
-    throw new Error(`No s'ha trobat cap departament amb el nom "${nomDepartament}"`);
-  }
-  return rows[0].DEPT_CODI;
+async function listAssigInfo() {
+  // Obtenir el codi del departament a partir del seu nom
+  const deptNom = 'Informàtica' & 'Matemàtica Aplicada';
+  const [deptResult] = await connection.execute(
+    'SELECT DEPT_CODI FROM departament WHERE DEPT_NOM = ?',
+    [deptNom]
+  );
+  const deptCodi = deptResult.length > 0 ? deptResult[0].DEPT_CODI : -1;
+
+  // Obtenir la informació de les assignatures impartides pels professors del departament
+  const [result] = await connection.execute(`
+    SELECT DISTINCT assig.ASSIG_CODI, assig.ASSIG_NOM
+    FROM assignatures assig
+    JOIN assignatures_professor ap ON assig.ASSIG_CODI = ap.ASSIGPROF_ASSIG_CODI
+    JOIN professor prof ON ap.ASSIGPROF_PROF_DNI = prof.PROF_DNI
+    WHERE prof.PROF_DEPT_CODI = ?
+  `, [deptCodi]);
+
+  // Tancar la connexió amb la base de dades
+  connection.end();
+
+  return result;
 }
 
-// Mètode per obtenir les assignatures que imparteixen els professors del departament de Informàtica i Matemàtica Aplicada
-async function getAssignaturesInformaticaIMatematicaAplicada(connection) {
-  const codiDepartament = await getCodiDepartament('Informàtica i Matemàtica Aplicada', connection);
+//Exercici 3
 
-  const [rows] = await connection.execute(`
-    SELECT DISTINCT assignatures.codi, assignatures.nom
-    FROM assignatures
-    JOIN professors_assignatures ON assignatures.codi = professors_assignatures.codi_assignatura
-    JOIN professors ON professors_assignatures.id_professor = professors.id
-    WHERE professors.codi_departament = ?
-  `, [codiDepartament]);
+function afegirDepartament(nom, ubicacio, telefon, dniProfessor) {
+  const data = {
+    nom: nom,
+    ubicacio: ubicacio,
+    telefon: telefon,
+    dniProfessor: dniProfessor
+  };
 
-  return rows;
+  fetch('/api/departament', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('No puc, pelacastanyes!');
+      }
+      return response.json();
+    })
+    .then(result => {
+      console.log('Nou departament afegit:', result);
+      // Aquí pots afegir codi per actualitzar la vista o fer alguna acció addicional
+    })
+    .catch(error => {
+      console.error('Error afegint departament:', error);
+      // Aquí pots afegir codi per mostrar un missatge d'error a l'usuari
+    });
 }
 
-// Mètode per llistar les assignatures que imparteixen els professors del departament de Informàtica i Matemàtica Aplicada
-app.get('/api/llistaAssiginfo', async (req, res) => {
-  try {
-    const connection = await mysql.createConnection(connectionConfig);
-    const assignatures = await getAssignaturesInformaticaIMatematicaAplicada(connection);
-    await connection.end();
 
-    res.json(assignatures);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error.message);
-  }
-});
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UF2ExHectorVinas';
+
+  constructor(private http: HttpClient) {
+    this.http.get('http://localhost:3080/assig').subscribe((result: any) => {
+    console.log(result);
+  });
+  }
+
 }
